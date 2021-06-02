cae1e16047303e7f6cf3541aac743ef58dfed06e
hanna_p <84588830+HPR-triangle@users.noreply.github.com>
2021-06-02 18:44:56 +0100

    Add files via upload
    Sleep descriptors added

--- src/descriptors.json.js
+++ src/descriptors.json.js
@@ -1,99 +1,42 @@
 export default {
 	"Sleep environment": {
 		__description: "",
-		"Self": null,
-		"Others in private": null,
-		"Others in public":  null,
+		"Room light": null,
+		"Room temperature": null,
+		"Bed and mattress":  null,
+		"Noise": null,
 	},
-	"Eating/drinking": {
-		"Inability to swallow": {
-			__description: " about what you have trouble swallowing",
-			"Dry mouth": null,
-			"Fluids":    null,
-			"Solids":    null,
-		},
-		"Loss of appetite":  null,
-		"Impaired taste/smell": null,
-		"Inability to chew": null,
-		"Embarrassment":     null,
-		"Voluntary/involuntary swallowing": null,
+	"Lifestyle": {
+		"Daytime stress": null, 
+		"Daytime screentime":  null,
+		"Daytime sleepiness": null,
+		"Diet": null,
+		"Medicine and stimulants":     null,
+		"Exercise": null,
 	},
-	"Fatigue": {
-		__description: " about how fatigue affects you",
-		"Physically": {
-			__description: " about what physical exhaustion prevents you from doing",
-			"Everyday stuff":      null,
-			"Gentle exercise":     null,
-			"Strenuous excercise": null,
-		},
-		"Mentally": {
-			__description: " about what mental exhaustion stops you from doing",
-			"Unable to do everyday stuff": null,
-			"Poor concentration": null,
-			"Brain fog": null,
-		},
-		"Emotionally": {
-			__description: " about which situations emotional fatigue affects you the most",
-			"Self":   null,
-			"Private/partner": null,
-			"Public": null,
-		},
+	"Sleep habits": {
+		__description: "",
+		"Daytime naps": null,
+		"Sleep rhythm": null,
+		"Screen time before sleep": null,
+		"Exercise before sleep": null,
+		"Sleep tracking": null,			
 	},
-	"Pain": {
-		"Severity": {
-			"Being dysfunctional": null,
-			"Sleep interruption":     null,
-			"Insufficient analgesia": null,
-			"Mood": null,
-		},
-		"Frequency": {
-			"Being dysfunctional": null,
-			"Sleep interruption":     null,
-			"Insufficient analgesia": null,
-			"Mood": null,
-		},
-		"Longevity": {
-			"Being dysfunctional": null,
-			"Sleep interruption":     null,
-			"Insufficient analgesia": null,
-			"Mood": null,
-		},
+	"Sleep quality": {
+		"Falling asleep": null,
+		"During sleep": null,
+		"Waking up": null,
 	},
-	"Intimacy/sex": {
-		"Self":    null,
-		"Partner": null,
-		"Others":   null,
+	"Sleep quantity": {
+		"Sleep time duration":    null,
+		"Falling asleep": null,
+		"Wakeups during sleep time":   null,
+		"Bed and wake times": null,
 	},
-	"Talking": {
-		"Family": {
-			"Exhausting":         null,
-			"Slurred":            null,
-			"Embarrassing":       null,
-			"Hard to understand": null,
-		},
-		"Friends": {
-			"Exhausting":         null,
-			"Slurred":            null,
-			"Embarrassing":       null,
-			"Hard to understand": null,
-		},
-		"Public": {
-			"Exhausting":         null,
-			"Slurred":            null,
-			"Embarrassing":       null,
-			"Hard to understand": null,
-		},
-		"Phone": {
-			"Exhausting":         null,
-			"Slurred":            null,
-			"Embarrassing":       null,
-			"Hard to understand": null,
-		},
-	},
-	"Work": {
-		"Ability": null,
-		"Desire":  null,
-		"Need":    null,
+	"Pre-sleep habits": {
+		"Media and technology": null,
+		"Sleep medication": null,
+		"Social activities/chores/work": null,		
 	},
 };
 
